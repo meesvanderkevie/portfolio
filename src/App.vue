b78e5b5d5bc0560b810b45c13f8ae2f404224fdd
ItzJustMees <85079315+meesvanderkevie@users.noreply.github.com>
2022-05-26 13:56:03 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <div id="app">
+    <h1>Mees van der Kevie</h1>
+  </div>
+</template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+
+  h1 {
+    font-size: 3rem;
+    font-weight: bold;
+    font-family: 'Open Sans', sans-serif;
+    text-align: center;
+  }
+</style>
